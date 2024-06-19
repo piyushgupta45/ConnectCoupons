c26ef8613435becc4c156095e7b345ead00b1645
Piyush <piyushgupta.pg1704@gmail.com>
2024-06-20 00:38:03 +0530

    s

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import "./login.scss";
+import './Login.scss';
 
 const Login = () => {
   const [email, setEmail] = useState("");
