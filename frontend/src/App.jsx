3e741a9cb498bbb044718e0ef90c956b2982a299
Piyush <piyushgupta.pg1704@gmail.com>
2024-06-20 00:46:47 +0530

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,15 +17,17 @@ const App = () => {
   return (
     // <Router>
       <div>
-        {localStorage.getItem('token') && showNavbar && <Navbar />}
-        <Routes>
-        <Route path="/" element={localStorage.getItem('token')? <Home /> :<Navigate to="/login" />}/>
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/user/profile" element={<Register />} />
-        </Routes>
-        {localStorage.getItem('token') && showNavbar && <Footer />}
+      {/* //   {localStorage.getItem('token') && showNavbar && <Navbar />}
+      //   <Routes>
+      //   <Route path="/" element={localStorage.getItem('token')? <Home /> :<Navigate to="/login" />}/>
+      //     <Route path="/user/:userId" element={<User />} />
+      //     <Route path="/login" element={<Login />} />
+      //     <Route path="/register" element={<Register />} />
+      //     <Route path="/user/profile" element={<Register />} />
+      //   </Routes>
+      
+      //   {localStorage.getItem('token') && showNavbar && <Footer />} */}
+      hello
       </div>
 
     // </Router>
